7649bea90721e07555291866906e5070cb7ca26c
meIwata <waleilulu@gmail.com>
2025-03-18 19:58:32 +0800

    update

---
+++ src/day01/ArrayDemo5.java
@@ -0,0 +1,22 @@
+package day01;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class ArrayDemo5 {
+
+	public static void main(String[] args) {
+		// 班上有10個同學
+		// 如何記錄每一個同學的成績
+		int[] scores = { 80, 70, 100, 90, 40, 60, 50, 30, 95, 55 };
+
+		// 請計算總分與平均
+		int sum = 0; // 用來做資料累計使用
+		for (int x : scores) {
+			System.out.println(x); // x表示每一筆分數
+			sum = sum + x;
+		}
+		System.out.println("--------------");
+		System.out.println("總分: " + sum);
+	}
+}
