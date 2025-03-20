1d8fc3453cf353fabfcf83da30e88e524e80d066
meIwata <waleilulu@gmail.com>
2025-03-20 18:55:19 +0800

    update

---
+++ src/day02/Coffee.java
@@ -0,0 +1,19 @@
+package day02;
+
+// 咖啡設計圖
+public class Coffee {
+	String name; // 品名
+	char size; // 尺寸(S, L)
+	int price; // 價格
+	boolean hot; // true: 熱 / false: 冷
+	
+	//建構子
+	public Coffee() {
+		
+	}
+
+	// 列印出產品資訊
+	void printinfo() {
+		System.out.printf("%c%s%s $%d\n", size, (hot ? "熱" : "冰"), name, price);
+	}
+}
