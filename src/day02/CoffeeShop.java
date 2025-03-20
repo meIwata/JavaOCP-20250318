7729db63bb6fc46ea6323e999fea2fa908cb2a48
meIwata <waleilulu@gmail.com>
2025-03-20 19:01:04 +0800

    update

--- src/day02/CoffeeShop.java
+++ src/day02/CoffeeShop.java
@@ -10,12 +10,19 @@ public class CoffeeShop {
 		latte.hot = true;
 		latte.printinfo();
 		
-		Coffee macchiato = new Coffee();
+		Coffee macchiato = new Coffee(); // 建立一個新物件
 		macchiato.name="焦糖瑪奇朵";
 		macchiato.size='大';
 		macchiato.price=80;
 		macchiato.hot = false;
 		macchiato.printinfo();
+		
+		Coffee americano = new Coffee(); // 建立一個新物件
+		americano.name = "美式";
+		americano.size = '大';
+		americano.price = 50;
+		americano.hot=true;
+		americano.printinfo();
 		
 	}
 
