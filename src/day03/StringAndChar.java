91a94ca898acf8c80859fa2690db7557635189d4
meIwata <waleilulu@gmail.com>
2025-03-25 18:26:35 +0800

    update

---
+++ src/day03/StringAndChar.java
@@ -0,0 +1,15 @@
+package day03;
+
+public class StringAndChar {
+
+	public static void main(String[] args) {
+		// 建立char
+		char c1 = 'A';
+		char c2 = 'A';
+
+		// 判斷是否相等?
+		System.out.println(c1 == c2); //true
+
+
+	}
+}
