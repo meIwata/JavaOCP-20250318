736e5ba06947b0a423666b79f02bf18baa0fb9a5
meIwata <waleilulu@gmail.com>
2025-03-25 18:50:56 +0800

    update

--- src/day03/StringAndChar.java
+++ src/day03/StringAndChar.java
@@ -8,8 +8,13 @@ public class StringAndChar {
 		char c2 = 'A';
 
 		// 判斷是否相等?
-		System.out.println(c1 == c2); //true
+		System.out.println(c1 == c2); // true (== 是用來比較stack內容)
 
-
+		// 建立字串
+		String s1 = new String("Java");
+		String s2 = new String("Java");
+		System.out.println(s1 == s2); 
+		System.out.println(s1.equals(s2)); // 有分大小寫的比較
+		System.out.println(s1.equalsIgnoreCase(s2)); // 不分大小寫得比較
 	}
 }
