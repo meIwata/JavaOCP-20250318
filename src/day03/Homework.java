90f2e596075454deb518fd4d26d3bd114720a2d7
meIwata <waleilulu@gmail.com>
2025-03-25 21:22:51 +0800

    update

---
+++ src/day03/Homework.java
@@ -0,0 +1,13 @@
+package day03;
+
+public class Homework {
+
+	public static void main(String[] args) {
+		double[][] values = { { 170.0, 60.5 }, { 182.5, 72.5 }, { 160.0, 45.5 }, { 175.0, 75.0 } };
+		//請求平均身高與體重
+		for(int i=0;i<values.length;i++) {
+			double h = 
+		}
+	}
+
+}
