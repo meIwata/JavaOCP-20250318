4c16ab4b99659511879c31fba1250b5dda0eab97
meIwata <waleilulu@gmail.com>
2025-03-25 19:07:02 +0800

    update

--- src/day03/StringAndChar.java
+++ src/day03/StringAndChar.java
@@ -10,11 +10,21 @@ public class StringAndChar {
 		// 判斷是否相等?
 		System.out.println(c1 == c2); // true (== 是用來比較stack內容)
 
-		// 建立字串
+		// 建立字串 1
 		String s1 = new String("Java");
 		String s2 = new String("Java");
-		System.out.println(s1 == s2); 
+		System.out.println(s1 == s2);
 		System.out.println(s1.equals(s2)); // 有分大小寫的比較
 		System.out.println(s1.equalsIgnoreCase(s2)); // 不分大小寫得比較
+
+		System.out.println("------------------------------");
+
+		// 建立字串 2，String Pool機制
+		String s3 = "Java"; // 字面值宣告 (字串內容可以共用)
+		String s4 = "Java"; // 字面值宣告 (字串內容可以共用)
+		System.out.println(s3 == s4); // true(共用)
+		System.out.println(s3.equals(s4));
+
+		// 總結: 在實務上不應用 == 來比較字串而是用equals()來進行比較
 	}
 }
---
+++ src/day03/StringDemo.java
@@ -0,0 +1,18 @@
+package day03;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+		// 字串 concat的應用
+		String s1 = "Java";
+		s1 = s1.concat("8");
+		System.out.println(s1);
+		
+		
+		String s2 = "Java";
+		s2.concat("11"); // 並沒有把值賦予給s2
+		System.out.println(s2);
+
+	}
+
+}
