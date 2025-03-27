9467237f4ffd91b59fd6ef07d885ce492bc6a6aa
meIwata <waleilulu@gmail.com>
2025-03-27 19:35:55 +0800

    update

--- src/day04/Book.java
+++ src/day04/Book.java
@@ -3,16 +3,31 @@ package day04;
 //書的設計
 public class Book {
 	String name; // 書名
-	int price; // 價格
+	private int price; // 價格
 	int pages; // 頁數
 
-	//若沒寫建構子，則Java會自動產生，所產生的建構子稱為預設建構子，其結構如下所述:
-	//無參數建構子
+	// 若沒寫建構子，則Java會自動產生，所產生的建構子稱為預設建構子，其結構如下所述:
+	// 無參數建構子
 	public Book() {
 		System.out.println("呼叫Book()建構子");
 	}
-	//寫一個printInfo()的方法
-	public void printInfo(){
+
+	// 封裝 Encapsulation setter
+	public void setPrice(int price) { // 輸入的價格price
+		if (price > 0) { // price>0
+			this.price = price; // 就把 price的值賦予給price
+		} else {
+			System.out.println("價格輸入錯誤");
+		}
+	}
+	// 封裝 Encapsulation getter
+	public int getPrice() {
+		// return this.price;
+		return price;
+	}
+
+	// 寫一個printInfo()的方法
+	public void printInfo() {
 		System.out.printf("%s $%d %d頁%n", name, price, pages);
 	}
 }
--- src/day04/BookStore.java
+++ src/day04/BookStore.java
@@ -5,9 +5,12 @@ public class BookStore {
 	public static void main(String[] args) {
 		Book book1 = new Book();
 		book1.name = "現代Java";
-		book1.price = 450;
+		book1.setPrice(450);
 		book1.pages = 325;
 		book1.printInfo(); //呼叫方法
+		
+		//單獨可以印出價格
+		System.out.println(book1.getPrice());
 	}
 
 }
