f0471f52cef73bbbe9b7aca6cb9789f82294d5c0
meIwata <waleilulu@gmail.com>
2025-03-27 19:56:13 +0800

    update

--- src/day04/Book.java
+++ src/day04/Book.java
@@ -2,14 +2,39 @@ package day04;
 
 //書的設計
 public class Book {
-	String name; // 書名
+	private String name; // 書名
 	private int price; // 價格
-	int pages; // 頁數
+	private int pages; // 頁數
 
+	//建構子封裝 (Constructor Encapsulation)
 	// 若沒寫建構子，則Java會自動產生，所產生的建構子稱為預設建構子，其結構如下所述:
 	// 無參數建構子
 	public Book() {
 		System.out.println("呼叫Book()建構子");
+	}
+
+	// 有參數建構子
+	public Book(String name, int price, int pages) {
+		// 傳統方法，但不能使用封裝裡面的判斷
+		// this.name = name;
+		// this.price = price;
+		// this.pages=pages;
+
+		// 使用set還可以使用封裝的判斷
+		setName(name);
+		setPrice(price);
+		setPages(pages);
+	}
+	
+	
+	
+	// 方法封裝 (Method Encapsulation)
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
 	}
 
 	// 封裝 Encapsulation setter
@@ -20,10 +45,23 @@ public class Book {
 			System.out.println("價格輸入錯誤");
 		}
 	}
+
 	// 封裝 Encapsulation getter
 	public int getPrice() {
 		// return this.price;
 		return price;
+	}
+
+	public void setPages(int pages) {
+		if (pages > 0) {
+			this.pages = pages;
+		} else {
+			System.out.println("頁數輸入錯誤");
+		}
+	}
+
+	public int getPages() {
+		return pages;
 	}
 
 	// 寫一個printInfo()的方法
--- src/day04/BookStore.java
+++ src/day04/BookStore.java
@@ -4,13 +4,17 @@ public class BookStore {
 
 	public static void main(String[] args) {
 		Book book1 = new Book();
-		book1.name = "現代Java";
+		book1.setName("現代Java");
 		book1.setPrice(450);
-		book1.pages = 325;
+		book1.setPages(325);
+		
 		book1.printInfo(); //呼叫方法
 		
 		//單獨可以印出價格
 		System.out.println(book1.getPrice());
+	
+		Book book2 = new Book("唐詩300首", 300, 150);
+		book2.printInfo();
 	}
 
 }
