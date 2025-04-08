96691d74f2784b925d514e559984c54f8faa5fbc
meIwata <waleilulu@gmail.com>
2025-04-08 20:01:33 +0800

    update

--- src/day06/Checkout.java
+++ src/day06/Checkout.java
@@ -2,8 +2,23 @@ package day06;
 
 // 結帳
 public class Checkout {
+	
+	//單例Singleton
+	// 自己生成靜態物件(不給使用者new)
+	private static Checkout checkout = new Checkout();
+	
+	// private建構子
+	private Checkout() {
+		
+	}
+	
+	// 提供一個public static方法將 checkout 物件給使用者
+	public static Checkout getInstance() {
+		return checkout;
+	}
+	
 	public void submit(int cash) {
-		System.out.printf("現金:%d 結帳%n ", cash);
+		System.out.printf("現金:%d 結帳%n", cash);
 	}
 	
 	public void submit(int cash, String qrcode) {
