c91925f29e8c629d5a3a4b38835458df35e63f5d
meIwata <waleilulu@gmail.com>
2025-04-08 18:50:17 +0800

    update

--- src/day06/Util.java
+++ src/day06/Util.java
@@ -3,15 +3,16 @@ package day06;
 // 工具
 public class Util {
 	// add這個方法就是 方法重載（Method Overloading）
-	public int add(int x, int y) {
-		return x + y;
-	}
-
-	public int add(int x, int y, int z) {
-		return x + y + z;
-	}
+//	public int add(int x, int y) {
+//		return x + y;
+//	}
+//
+//	public int add(int x, int y, int z) {
+//		return x + y + z;
+//	}
 	
-	public int add(int[] array) {
+//	public int add(int[] array) {
+	public int add(int... array) { // Java 5 開始引入的 可變參數（Varargs） 語法
 		int total =0;
 		for(int row: array) {
 			total +=row;
--- src/day06/UtilTest.java
+++ src/day06/UtilTest.java
@@ -5,13 +5,16 @@ public class UtilTest {
 	public static void main(String[] args) {
 		// 建立工具物件
 		Util util = new Util();
-		
+
 		// 進行add計算並可以得到結果後印出
 		System.out.println(util.add(10, 20));
 		System.out.println(util.add(10, 20, 30));
-		
-		int []array= {10,20,30,40,50};
+
+		int[] array = { 10, 20, 30, 40, 50 };
 		System.out.println(util.add(array));
+
+		System.out.println(util.add());
+		System.out.println(util.add(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
 	}
 
 }
