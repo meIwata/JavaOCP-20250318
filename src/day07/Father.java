78996f832d307cf53a46105f96eb74b43e282915
meIwata <waleilulu@gmail.com>
2025-04-10 19:05:30 +0800

    update

---
+++ src/day07/Father.java
@@ -0,0 +1,8 @@
+package day07;
+
+public class Father {
+	
+	int cash = 1000000; // 現金
+	protected int stock = 10000000; // 股票
+	
+}
---
+++ src/day07/SonTest.java
@@ -0,0 +1,10 @@
+package day07;
+
+public class SonTest {
+
+	public static void main(String[] args) {
+		Son son = new Son();
+		son.info();
+	}
+
+}
